c04453cf259f5132076437df887ead4fc7655b0d
Elena Andreeva <elena.and.kam@gmail.com>
2024-11-04 23:31:52 +0300

    add solutions for 2 number tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -394,8 +394,8 @@ function toExponential(number, fractionDigits) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  return `${number.toFixed(fractionDigits)}`;
 }
 
 /**
@@ -410,8 +410,8 @@ function toFixed(/* number, fractionDigits */) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  return number.toFixed(precision - Math.trunc(number).toString().length);
 }
 
 /**
@@ -443,8 +443,12 @@ function getNumberValue(/* number */) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  return (
+    typeof number === 'number' &&
+    !Number.isNaN(number) &&
+    Number.isFinite(number)
+  );
 }
 
 /**
